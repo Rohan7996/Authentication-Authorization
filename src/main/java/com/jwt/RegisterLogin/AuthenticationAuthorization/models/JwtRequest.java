12902017d2c0f7c471c2212ee11841bdd7d3197a
Rohan7996 <116304597+Rohan7996@users.noreply.github.com>
2024-05-20 23:27:22 +0530

    Update JwtRequest.java

--- src/main/java/com/jwt/RegisterLogin/AuthenticationAuthorization/models/JwtRequest.java
+++ src/main/java/com/jwt/RegisterLogin/AuthenticationAuthorization/models/JwtRequest.java
@@ -6,12 +6,16 @@ import lombok.*;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Builder
 @ToString
 public class JwtRequest {
     private String username;
     private String password;
+
+    public JwtRequest(String username, String password) {
+        this.username=username;
+        this.password=password;
+    }
 
     public String getUsername() {
         return username;
